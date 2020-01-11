08ab4e0384d634bc82c558cea11301c2a561c9fe
westonludeke <westonludeke@gmail.com>
2020-01-11 11:49:11 -0600

    Uploading Rock, Paper, Scissors, Spock, Lizard game

---
+++ rock_paper_scissors/rps_spock_lizard.rb
@@ -0,0 +1,54 @@
+VALID_CHOICES = %w(rock paper scissors lizard spock)
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+def win?(first, second)
+  (first == 'scissors' && second == 'paper') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'rock' && second == 'lizard') ||
+    (first == 'lizard' && second == 'spock') ||
+    (first == 'spock' && second == 'scissors') ||
+    (first == 'scissors' && second == 'lizard') ||
+    (first == 'lizard' && second == 'paper') ||
+    (first == 'paper' && second == 'spock') ||
+    (first == 'spock' && second == 'rock') ||
+    (first == 'rock' && second == 'scissors')
+end
+
+def display_results(player, computer)
+  if win?(player, computer)
+    prompt('You won!')
+  elsif win?(computer, player)
+    prompt('Computer won!')
+  else
+    prompt("It's a draw! Tie game.")
+  end
+end
+
+loop do
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = Kernel.gets.chomp
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  display_results(choice, computer_choice)
+
+  prompt("You chose #{choice}. The computer chose #{computer_choice}")
+
+  prompt('Do you want to play again?')
+  answer = Kernel.gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt('Thank you for playing. Good bye!')
