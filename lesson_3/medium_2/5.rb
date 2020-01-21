1d3a7c8d68275e85d9109a560307320e31c12b83
westonludeke <westonludeke@gmail.com>
2020-01-21 10:46:52 -0600

    finished medium 2

---
+++ lesson_3/medium_2/5.rb
@@ -0,0 +1,17 @@
+# Depending on a method to modify its arguments can be tricky:
+
+def tricky_method(a_string_param, an_array_param)
+  a_string_param += "rutabaga"
+  an_array_param += ["rutabaga"]
+end
+
+my_string = "pumpkins"
+my_array = ["pumpkins"]
+tricky_method(my_string, my_array)
+
+puts "My string looks like this now: #{my_string}"
+puts "My array looks like this now: #{my_array}"
+
+# Whether the above "coincidentally" does what we think we wanted "depends" upon what is going on inside the method.
+
+# How can we refactor this practice problem to make the result easier to predict and easier for the next programmer to maintain?
---
+++ lesson_3/medium_2/6.rb
@@ -0,0 +1,16 @@
+# How could the unnecessary duplication in this method be removed?
+
+# def color_valid(color)
+#   if color == "blue" || color == "green"
+#     true
+#   else
+#     false
+#   end
+# end
+
+def color_valid(color)
+  color == "blue" || color == "green"
+end
+
+puts color_valid("blue")
+puts color_valid("red")
