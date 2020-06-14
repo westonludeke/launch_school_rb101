541e177f359ce067cc0e68588edbae8319141bbd
westonludeke <westonludeke@gmail.com>
2020-06-14 14:16:41 -0500

    Updated methods for twenty_one

--- 6_lesson/twenty_one.rb
+++ 6_lesson/twenty_one.rb
@@ -1,68 +1,96 @@
-# Assignment: Twenty-One
+# Twenty-One
+card_values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace']
+card_suits = ['hearts', 'diamonds', 'club', 'spade']
 
-cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace']
+keep_score = {'player_score_current_hand' => 0, 'dealer_score_current_hand' => 0, \
+              'player_game_score' => 0, 'dealer_game_score' => 0}
 
-face_values = ['hearts', 'diamonds', 'club', 'spade']
+player_hand = []
+dealer_hand = []
+deck_of_cards = []
+player_current_card_value = []
+dealer_current_card_value = []
 
-player_cards = []
-dealer_cards = []
-remaining_cards = []
-
-player_cards_total_value = 0
-dealer_cards_total_value = 0
+# player_hand_total_value = 0
+# dealer_hand_total_value = 0
 
 # Create Deck of all Cards
-face_values.each do |val|
-  i = 0
-  until i == cards.length do 
-    remaining_cards << [val, cards[i]]
-    i += 1
+def create_deck(card_suits, card_values, deck_of_cards)
+  card_suits.each do |val|
+    i = 0
+    until i == card_values.length do 
+      deck_of_cards << [val, card_values[i]]
+      i += 1
+    end
   end
 end
-# Shuffle the deck and draw cards
-def shuffle_and_draw
-  shuffled_cards = remaining_cards.shuffle!
-  p "The deck contains #{remaining_cards.length} cards."
-  player_cards << remaining_cards.pop
-  dealer_cards << remaining_cards.pop
+create_deck(card_suits, card_values, deck_of_cards)
+p deck_of_cards
 
-  p "The player has: #{player_cards}"
-  p "The dealer has: #{dealer_cards}"
+# Shuffle the deck and deal cards
+def shuffle_and_deal(deck_of_cards, player_hand, dealer_hand)
+  deck_of_cards = deck_of_cards.shuffle!
+  p "The deck contains #{deck_of_cards.length} cards to start."
+  player_hand << deck_of_cards.pop
+  dealer_hand << deck_of_cards.pop
+
+  p "The player has: #{player_hand}"
+  p "The dealer has: #{dealer_hand}"
 end
-shuffle_and_draw
-# Calculate points for player
-player_point = player_cards[0][1]
+shuffle_and_deal(deck_of_cards, player_hand, dealer_hand)
 
-if player_point == 'ace' 
-  if player_cards_total_value + 11 <= 21
-    player_cards_total_value += 11
+# Getting the value of player and dealer's currently dealt card
+def get_current_card_value(player_hand, dealer_hand, player_current_card_value, dealer_current_card_value) 
+  player_current_card_value << player_hand[0][1]
+  dealer_current_card_value << dealer_hand[0][1]
+  p player_current_card_value
+end
+get_current_card_value(player_hand, dealer_hand, player_current_card_value, dealer_current_card_value)
+
+p player_current_card_value[0]
+p dealer_current_card_value[0]
+
+def calculate_player_hand_points(player_current_card_value, keep_score)
+  if player_current_card_value[0] == 'ace' 
+    if keep_score['player_score_current_hand'] + 11 <= 21
+      keep_score['player_score_current_hand'] += 11
+    else
+      keep_score['player_score_current_hand'] += 1
+    end
+  elsif player_current_card_value[0] == 'jack' || player_current_card_value[0] == 'queen' || player_current_card_value[0] == 'king'
+    keep_score['player_score_current_hand'] += 10
+  elsif  player_current_card_value[0].is_a? Integer
+    keep_score['player_score_current_hand'] += player_current_card_value[0]
   else
-    player_cards_total_value += 1
+    p "invalid"
   end
-elsif player_point == 'jack' || player_point == 'queen' || player_point == 'king'
-  player_cards_total_value += 10
-elsif  player_point.is_a? Integer
-  player_cards_total_value += player_point
+  p keep_score['player_score_current_hand']
 end
+calculate_player_hand_points(player_current_card_value, keep_score)
+
+p keep_score['player_score_current_hand']
 
 
-# Calculate points for dealer
-dealer_point = dealer_cards[0][1]
-
-if dealer_point == 'ace'
-  if dealer_cards_total_value + 11 <= 21
-    dealer_cards_total_value += 11
+def calculate_dealer_hand_points(dealer_current_card_value, keep_score)
+  if dealer_current_card_value[0] == 'ace' 
+    if keep_score['dealer_score_current_hand'] + 11 <= 21
+      keep_score['dealer_score_current_hand'] += 11
+    else
+      keep_score['dealer_score_current_hand'] += 1
+    end
+  elsif dealer_current_card_value[0] == 'jack' || dealer_current_card_value[0] == 'queen' || dealer_current_card_value[0] == 'king'
+    keep_score['dealer_score_current_hand'] += 10
+  elsif  dealer_current_card_value[0].is_a? Integer
+    keep_score['dealer_score_current_hand'] += dealer_current_card_value[0]
   else
-    dealer_cards_total_value += 1
+    p "invalid"
   end
-elsif dealer_point == 'jack' || dealer_point == 'queen' || dealer_point == 'king'
-  dealer_cards_total_value += 10
-elsif dealer_point.is_a? Integer
-  dealer_cards_total_value += dealer_point
+  p keep_score['dealer_score_current_hand']
 end
+calculate_dealer_hand_points(dealer_current_card_value, keep_score)
 
-p "Player you have a #{player_cards_total_value} and the dealer has a #{dealer_cards_total_value}."
-p "The deck now contains #{remaining_cards.length} cards."  
+p keep_score['dealer_score_current_hand']
+
 
 
 =begin Implementation Steps:
@@ -77,6 +105,6 @@ p "The deck now contains #{remaining_cards.length} cards."
 6. If dealer bust, player wins.
 7. Compare cards and declare winner.
 
-You start with a normal 52-card deck consisting of the 4 suits (hearts, diamonds, clubs, and spades), and 13 values (2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace).
+You start with a normal 52-card deck consisting of the 4 card_suits (hearts, diamonds, clubs, and spades), and 13 values (2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace).
 
 =end
