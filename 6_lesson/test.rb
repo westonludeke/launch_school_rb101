4a3b296772b126836f2d699b25f731e2ff8dcdf2
westonludeke <westonludeke@gmail.com>
2020-06-18 10:56:32 -0500

    refactoring twenty-one

--- 6_lesson/test.rb
+++ 6_lesson/test.rb
@@ -1,21 +1,139 @@
+# Twenty-One
+
+=begin ----TO-DO LIST----
+
+1. Both users are initially dealt two cards. 
+2. Player sees both of their cards.
+3. Player can only see one of the dealers cards, all others are "unknown".
+4. The value of Ace changes each time a new card is drawn.
+  4b. When a new card is drawn, the value of ace is determined based on if all cards are >= 21.
+5. Player continues to make choices until: 
+  * Reaching 21, busts (i.e. over 21), or the player stays (i.e. under 21).
+6. The dealer only draws more cards once the player stays.
+7. If the dealer stays, compare points to see has the higher total.
+8. The dealer hits until the dealer's points are at least 17.
+9. If the dealer busts, the player wins.
+
+=end
+
 card_values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace']
-card_suits = ['hearts', 'diamonds', 'club', 'spade']
+card_suits = ['hearts', 'diamonds', 'clubs', 'spades']
 
-keep_score = { 'player_current_card_value' => 'ace', \
-               'dealer_current_card_value' => 'ace', \
-               'player_hand_points' => 1, \
-               'dealer_hand_points' => 10, \
-               'player_game_score' => 0, \
-               'dealer_game_score' => 0}
+keep_score = { 'player_cards_converted' => 0, \
+               'dealer_cards_converted' => 0, \
+               'player_cards' => [], \
+               'dealer_cards' => [], \
+               'player_card_value' => [], \
+               'dealer_card_value' => [], \
+               'player_points' => 0, \
+               'dealer_points' => 0}
 
+deck_of_cards = []
 
-
-
-def get_current_card_value(player_hand, dealer_hand, keep_score)
-  player_hand["jack", 10]
-  dealer_hand[]
-  # -- Add these to an array inside the hash
-  player_hand.shift
-  dealer_hand.shift
+def welcome
+  system('clear') || system('cls')
+  puts "Welcome to Twenty-One!"
+  puts "The dealer is shuffling the deck and will soon hand out two cards each."
+  #sleep 3
+  system('clear') || system('cls')
 end
-get_current_card_value(player_hand, dealer_hand, keep_score)
+welcome
+
+def create_deck(card_suits, card_values, deck_of_cards)
+  card_suits.each do |val|
+    i = 0
+    until i == card_values.length do 
+      deck_of_cards << [val, card_values[i]]
+      i += 1
+    end
+  end
+end
+create_deck(card_suits, card_values, deck_of_cards)
+
+def shuffle!(deck_of_cards)
+  deck_of_cards = deck_of_cards.shuffle!
+end
+
+def deal_first_two_cards_to_each_user(deck_of_cards, keep_score)
+  shuffle!(deck_of_cards)
+  keep_score["player_cards"] << deck_of_cards.pop
+  keep_score["dealer_cards"] << deck_of_cards.pop
+end
+deal_first_two_cards_to_each_user(deck_of_cards, keep_score)
+deal_first_two_cards_to_each_user(deck_of_cards, keep_score)
+
+p keep_score
+
+def show_player_their_two_cards(keep_score)
+  puts "Player you have a #{keep_score["player_cards"][0][1]} of #{keep_score["player_cards"][0][0]} " \
+  "and a #{keep_score["player_cards"][1][1]} of #{keep_score["player_cards"][1][0]}"
+end
+show_player_their_two_cards(keep_score)
+
+def show_dealer_first_card(keep_score)
+ puts "The dealer has a #{keep_score["dealer_cards"][0][1]} of #{keep_score["dealer_cards"][0][0]} " \
+"and #{keep_score["player_cards"].length - 1} unknown cards."
+end
+show_dealer_first_card(keep_score)
+
+
+
+def get_current_player_card_value(keep_score)
+  #puts keep_score['player_cards'][keep_score['player_cards_converted']][1]
+  keep_score['player_card_value'] = keep_score['player_cards'][keep_score['player_cards_converted']][1]
+  keep_score['player_cards_converted'] += 1
+end
+get_current_player_card_value(keep_score)
+
+p keep_score
+
+def convert_ace_to_one_or_eleven(keep_score)
+  if keep_score['player_card_value'] == 'ace'
+    if keep_score['player_points'] + 11 <= 21
+      keep_score['player_points'] += 11
+    else
+      keep_score['player_points'] += 1
+    end
+  end
+end
+convert_ace_to_one_or_eleven(keep_score)
+
+p keep_score
+#   if keep_score['player_card'] == 'ace'
+#     if keep_score['player_hand_points'] + 11 <= 21
+#       keep_score['player_hand_points'] += 11
+#     else 
+#       keep_score['player_hand_points'] += 1
+#     end
+#   end
+
+#   if keep_score['dealer_current_card_value'] == 'ace'
+#     if keep_score['dealer_hand_points'] + 11 <= 21
+#       keep_score['dealer_hand_points'] += 11
+#     else 
+#       keep_score['dealer_hand_points'] += 1
+#     end
+#   end  
+# end
+
+# def face_card_check
+# end
+
+# def add_points
+# end
+
+=begin Implementation Steps:
+
+1. Initialize deck
+2. Deal cards to player and dealer
+3. Player turn: hit or stay
+  - repeat until bust or "stay"
+4. If player bust, dealer wins.
+5. Dealer turn: hit or stay
+  - repeat until total >= 17
+6. If dealer bust, player wins.
+7. Compare cards and declare winner.
+
+You start with a normal 52-card deck consisting of the 4 card_suits (hearts, diamonds, clubs, and spades), and 13 values (2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace).
+
+=end
--- 6_lesson/twenty_one.rb
+++ 6_lesson/twenty_one.rb
@@ -1,10 +1,28 @@
 # Twenty-One
+
+=begin ----TO-DO LIST----
+
+1. Both users are initially dealt two cards. 
+2. Player sees both of their cards.
+3. Player can only see one of the dealers cards, all others are "unknown".
+4. The value of Ace changes each time a new card is drawn.
+  4b. When a new card is drawn, the value of ace is determined based on if all cards are >= 21.
+5. Player continues to make choices until: 
+  * Reaching 21, busts (i.e. over 21), or the player stays (i.e. under 21).
+6. The dealer only draws more cards once the player stays.
+7. If the dealer stays, compare points to see has the higher total.
+8. The dealer hits until the dealer's points are at least 17.
+9. If the dealer busts, the player wins.
+
+=end
+
 card_values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace']
 card_suits = ['hearts', 'diamonds', 'clubs', 'spades']
 
 keep_score = { 'player_current_card_value' => '', \
                'dealer_current_card_value' => '', \
-               'player_holds' => [], \
+               'player_current_hand' => [], \
+               'dealer_current_hand' => [], \
                'player_hand_points' => 0, \
                'dealer_hand_points' => 0, \
                'player_game_score' => 0, \
@@ -14,10 +32,12 @@ player_hand = []
 dealer_hand = []
 deck_of_cards = []
 
+# Beginning & Creating the Deck
 def welcome
+  system('clear') || system('cls')
   puts "Welcome to Twenty-One!"
-  puts "The dealer is shuffling the deck now and will soon hand out the first cards."
-  sleep 3
+  puts "The dealer is shuffling the deck and will soon hand out two cards each."
+  sleep 5
   system('clear') || system('cls')
 end
 welcome
@@ -33,27 +53,36 @@ def create_deck(card_suits, card_values, deck_of_cards)
 end
 create_deck(card_suits, card_values, deck_of_cards)
 
-def shuffle_and_deal(deck_of_cards, player_hand, dealer_hand, keep_score)
+def shuffle_and_deal_both_users(deck_of_cards, player_hand, dealer_hand, keep_score)
   deck_of_cards = deck_of_cards.shuffle!
   player_hand << deck_of_cards.pop
-  keep_score["player_holds"] << player_hand[0]
+  keep_score["player_current_hand"] << player_hand[0]
   dealer_hand << deck_of_cards.pop
+  keep_score["dealer_current_hand"] << dealer_hand[0]
 end
-shuffle_and_deal(deck_of_cards, player_hand, dealer_hand, keep_score)
 
 def shuffle_and_deal_player_only(deck_of_cards, player_hand, keep_score)
   deck_of_cards = deck_of_cards.shuffle!
   player_hand << deck_of_cards.pop
-  keep_score["player_holds"] << player_hand[0]
+  keep_score["player_current_hand"] << player_hand[0]
 end
+
+# def shuffle_and_deal_dealer_only(deck_of_cards, dealer_hand, keep_score)
+#   deck_of_cards = deck_of_cards.shuffle!
+#   dealer_hand << deck_of_cards.pop
+#   keep_score["dealer_current_hand"] << dealer_hand[0]
+# end
 
 def get_current_card_value(player_hand, dealer_hand, keep_score)
   keep_score['player_current_card_value'] = player_hand[0][1]
   keep_score['dealer_current_card_value'] = dealer_hand[0][1]
   player_hand.shift
-  #dealer_hand.shift
+  dealer_hand.shift
+  #p "player hand is #{player_hand}"
+  #p "dealer hand is #{dealer_hand}"
+  #p "player holds #{keep_score["player_current_hand"]}"
+  #p "dealer holds #{keep_score["dealer_current_hand"]}"
 end
-get_current_card_value(player_hand, dealer_hand, keep_score)
 
 def convert_ace_to_one_or_eleven(keep_score)
   if keep_score['player_current_card_value'] == 'ace'
@@ -72,7 +101,6 @@ def convert_ace_to_one_or_eleven(keep_score)
     end
   end  
 end
-convert_ace_to_one_or_eleven(keep_score)
 
 def convert_face_cards_to_ten(keep_score)
   if keep_score['player_current_card_value'] == 'jack' || \
@@ -88,7 +116,6 @@ def convert_face_cards_to_ten(keep_score)
     keep_score['dealer_hand_points'] += 10
   end
 end
-convert_face_cards_to_ten(keep_score)
 
 def add_integer_points(keep_score) 
   if keep_score['player_current_card_value'].is_a? Integer
@@ -99,36 +126,47 @@ def add_integer_points(keep_score)
     keep_score['dealer_hand_points'] += keep_score['dealer_current_card_value']
   end
 end
-add_integer_points(keep_score)
 
+#shuffle_and_deal_both_users(deck_of_cards, player_hand, dealer_hand, keep_score)
 p keep_score
 
 def show_the_player_what_cards_theyre_holding(keep_score)
-  if keep_score["player_holds"].length == 1
-    keep_score["player_holds"].each do |card|
-      puts "You are holding a #{card[1]} of #{card[0]}."
-    end
-  else
-    keep_score["player_holds"].each do |card|
-      puts "You are holding a #{card[1]} of #{card[0]}"
-    end
+  keep_score["player_current_hand"].each do |card|
+    puts "You have a #{card[1]} of #{card[0]}"
   end  
 end
 
-def alert_first_dealt_cards(keep_score)
-  puts "The dealer has a: #{keep_score['dealer_current_card_value']}."
-  show_the_player_what_cards_theyre_holding(keep_score)
-end
-alert_first_dealt_cards(keep_score)
+def display_dealer_first_two_cards(keep_score)
+  puts "The dealer has a #{keep_score['dealer_current_hand'][0][1]} of " \
+  "#{keep_score['dealer_current_hand'][0][0]}"
 
+  if keep_score["dealer_current_hand"].length > 1
+    unknown_cards = (keep_score["dealer_current_hand"].length - 1)
+    puts "The dealer also has #{unknown_cards} unknown cards."
+  end
+end
+
+def deal_two_cards_both_users(deck_of_cards, player_hand, dealer_hand, keep_score)
+  shuffle_and_deal_both_users(deck_of_cards, player_hand, dealer_hand, keep_score)
+  get_current_card_value(player_hand, dealer_hand, keep_score)
+  convert_ace_to_one_or_eleven(keep_score)
+  convert_face_cards_to_ten(keep_score)
+  add_integer_points(keep_score)
+  show_the_player_what_cards_theyre_holding(keep_score)
+  display_dealer_first_two_cards(keep_score)
+end
+deal_two_cards_both_users(deck_of_cards, player_hand, dealer_hand, keep_score)
+
+
+ # ------
 def player_hits(keep_score, deck_of_cards, player_hand, dealer_hand)
-  puts "Drawing a new card now..."
   # shuffle_and_deal(deck_of_cards, player_hand, dealer_hand, keep_score)
   shuffle_and_deal_player_only(deck_of_cards, player_hand, keep_score)
   get_current_card_value(player_hand, dealer_hand, keep_score)
   convert_ace_to_one_or_eleven(keep_score)
   convert_face_cards_to_ten(keep_score)
   add_integer_points(keep_score)
+  display_dealer_first_two_cards(keep_score)
   show_the_player_what_cards_theyre_holding(keep_score)
   p keep_score
 end
@@ -162,6 +200,10 @@ def ask_player_next_move(keep_score, deck_of_cards, player_hand, dealer_hand)
   puts "Would you like to hit or stay?"
   @answer = gets.chomp
   puts "You have decided to #{@answer}"
+  if @answer != 'stay'
+    puts "Dealing a new card now..."
+    sleep 5
+  end
 end
 
 def stay_and_determine_winner(keep_score)
